e4fa05d33372671143deb377635d28b965e265f0
Nemanja <66609488+JovanaT99@users.noreply.github.com>
2023-05-29 21:10:46 +0200

    second commit

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,31 +1,442 @@
 import React from "react";
 import "./index.css"
 
-
-
-
-
 const Home = () => {
-
 
     return (
 
-        <div className="hero">
-            <div className="hero-content">
+        <div>
+            <div className="hero">
+                <div className="hero-content">
 
-                <h2><span className="text-primary">Strikingly fast</span> QR codes.</h2>
-                <p>You are one Bolt away from never being too slow again</p>
-                <button className="generateqr-button">Generate QR Codes</button>
-                <button className="signup-button">Sign up</button>
+                    <h2><span className="text-primary">Strikingly fast</span> QR codes.</h2>
+                    <p>You are one Bolt away from never being too slow again</p>
+                    <button className="generateqr-button">Generate QR Codes</button>
+                    <button className="signup-button">Sign up</button>
+                </div>
+
+                <img className="hero-image" src="/img/qr-code-purchase.png" alt="hero-img"></img>
+
             </div>
 
-<img className="hero-image" src="/img/qr-code-purchase.png" alt="hero-img"></img>
+            <section className="qr-generator">
+                <div className="row">
+
+                    <div className="col">
+                        <h4>QR code Generator.</h4>
+                        <p>Your not sure whether to use Dynamic or Static QR codes? Get familiar with both and choose wisely.</p>
+
+                    </div>
+                </div>
+                <div className="row">
+
+                    <div className="col">
+
+                        <iframe width="100%" height="283" src="https://www.youtube.com/embed/VWvkhPFKSxY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen style={{ borderRadius: 15 }}></iframe>
+
+                    </div>
+                    <div className="col dynamicqr-code">
+
+                        <h5><img src="/img/url.SVG" alt="url-icon" width="22"></img>Dynamic QR codes</h5>
+                        <p>Have you ever wanted to change the destination of your QR code?
+                            If your answer is yes, Dynamic QR codes are perfect for you. They allow you to change the destination of the QR code on-the-fly, whenever you want. It combines multiple technologies to make this happen in the background with the help of our Links tool.</p>
+                        <ul className="dynamicqr-list">
+
+                            <li>Advanced scan statistics</li>
+                            <li>Change URL destination dynamically</li>
+                            <li>Scheduling, expiration limits, A/B Rotation</li>
+                            <li>Password protection</li>
+                            <li>Country, device & language re-targeting</li>
+
+
+
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="row">
+
+
+                    <div className="col dynamicqr-code">
+
+                        <h5><img src="/img/static-icon.SVG" alt="static-icon" width="22"></img>Static QR codes</h5>
+                        <p>Ful customizable QR codes are something you will definitely need for your company.
+
+                            Static QR codes have no scan limit, also they are fixed, meaning that all the data is directly embedded into the QR code. Once generated and printed for real-world usage, it can't be changed. ƒ</p>
+                        <span>To summarize:</span>
+
+                        <ul className="dynamicqr-list">
+
+                            <li>Fully customizable</li>
+                            <li>No scan limit</li>
+                            <li>No data saved on our servers</li>
+                        </ul>
+                    </div>
+
+                    <div className="col">
+
+                        <iframe width="100%" height="283" src="https://www.youtube.com/embed/VWvkhPFKSxY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen style={{ borderRadius: 15 }}></iframe>
+
+                    </div>
+                </div>
+            </section>
+
+            <section className="features">
+
+                <div className="row">
+                    <div className="col">
+
+
+                        <h4>With other features.</h4>
+                        <p>From QR code templates to custom domains, Bolt offers you all the features that you might need.</p>
+                    </div>
+                </div>
+                <div className="features-list">
+                    <div className="row">
+
+                        <div className="feature-card">
+
+
+                            <div className="feature-title">
+
+                                QR templates
+                            </div>
+                            <p>You get 13 built-in QR code templates to get you started right away.</p>
+
+                        </div>
+                        <div className="feature-card">
+
+
+                            <div className="feature-title">
+
+                                QR templates
+                            </div>
+                            <p>You get 13 built-in QR code templates to get you started right away.</p>
+
+                        </div>
+
+                        <div className="feature-card">
+
+                            <div className="feature-title">
+
+                                QR templates
+                            </div>
+                            <p>You get 13 built-in QR code templates to get you started right away.</p>
+
+                        </div>
+                    </div>
+
+                    <div className="row">
+
+                        <div className="feature-card">
+
+
+                            <div className="feature-title">
+
+                                QR templates
+                            </div>
+                            <p>You get 13 built-in QR code templates to get you started right away.</p>
+
+                        </div>
+                        <div className="feature-card">
+
+
+                            <div className="feature-title">
+
+                                QR templates
+                            </div>
+                            <p>You get 13 built-in QR code templates to get you started right away.</p>
+
+                        </div>
+
+                        <div className="feature-card">
+
+                            <div className="feature-title">
+
+                                QR templates
+                            </div>
+                            <p>You get 13 built-in QR code templates to get you started right away.</p>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </section>
+
+            <section>
+                <div className="row">
+
+                    <div className="col">
+
+                        <h4>QR code types.</h4>
+                        <p>QR codes can be used for multiple purposes! So here you have a large array of QR code templates to choose from and get started.</p>
+
+
+                    </div>
+                </div>
+
+
+                <div className="qr-code-types">
+
+                    <div className="qr-type-card">
+
+                        <div className="qr-type-card-title  text-primary">
+                            Text
+                        </div>
+                        <p>Generate easy & customizable Text QR codes in minutes.</p>
+
+
+                    </div>
+
+                    <div className="qr-type-card">
+
+                        <div className="qr-type-card-title  text-primary">
+                            Text
+                        </div>
+                        <p>Generate easy & customizable Text QR codes in minutes.</p>
+
+
+                    </div>
+                    <div className="qr-type-card">
+
+                        <div className="qr-type-card-title  text-primary">
+                            Text
+                        </div>
+                        <p>Generate easy & customizable Text QR codes in minutes.</p>
+
+
+                    </div>
+                    <div className="qr-type-card">
+
+                        <div className="qr-type-card-title  text-primary">
+                            Text
+                        </div>
+                        <p>Generate easy & customizable Text QR codes in minutes.</p>
+
+
+                    </div>
+                    <div className="qr-type-card">
+
+                        <div className="qr-type-card-title  text-primary">
+                            Text
+                        </div>
+                        <p>Generate easy & customizable Text QR codes in minutes.</p>
+
+
+                    </div>
+                    <div className="qr-type-card">
+
+                        <div className="qr-type-card-title  text-primary">
+                            Text
+                        </div>
+                        <p>Generate easy & customizable Text QR codes in minutes.</p>
+
+
+                    </div>
+                    <div className="qr-type-card">
+
+                        <div className="qr-type-card-title  text-primary">
+                            Text
+                        </div>
+                        <p>Generate easy & customizable Text QR codes in minutes.</p>
+
+
+                    </div>
+                    <div className="qr-type-card">
+
+                        <div className="qr-type-card-title  text-primary">
+                            Text
+                        </div>
+                        <p>Generate easy & customizable Text QR codes in minutes.</p>
+
+
+                    </div>
+                    <div className="qr-type-card">
+
+                        <div className="qr-type-card-title  text-primary">
+                            Text
+                        </div>
+                        <p>Generate easy & customizable Text QR codes in minutes.</p>
+
+
+                    </div>
+                    <div className="qr-type-card">
+
+                        <div className="qr-type-card-title  text-primary">
+                            Text
+                        </div>
+                        <p>Generate easy & customizable Text QR codes in minutes.</p>
+
+
+                    </div>
+                    <div className="qr-type-card">
+
+                        <div className="qr-type-card-title  text-primary">
+                            Text
+                        </div>
+                        <p>Generate easy & customizable Text QR codes in minutes.</p>
+
+
+                    </div>
+                    <div className="qr-type-card">
+
+                        <div className="qr-type-card-title  text-primary">
+                            Text
+                        </div>
+                        <p>Generate easy & customizable Text QR codes in minutes.</p>
+
+
+                    </div>
+                    <div className="qr-type-card">
+
+                        <div className="qr-type-card-title  text-primary">
+                            <img src="/img/text.SVG" alt="text-svg" />Text
+                        </div>
+                        <p>Generate easy & customizable Text QR codes in minutes.</p>
+
+                    </div>
+
+                </div>
+
+            </section>
+
+
+            <section>
+
+                <div className="row">
+                    <div className="col">
+
+                        <h4>Simple, transparent pricing.</h4>
+                        <p>Not sure if you need Bolt? You can sign up for your free guest account to try Bolt out.
+
+                            If you decide that Bolt could be useful and that you need something more advanced choose the plan that is right for you and your budget.</p>
+
+                    </div>
+
+                    <div className="col">
+                        <div className="pricing-buttons">
+                            <button className="button-primary">Monthly</button>
+                            <button className="button-secondary">Annual</button>
+                            <button className="button-secondary">Lifetime</button>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="pricing-list">
+                    <div className="pricing-item">
+
+                        <div className="pricing-item-heading">GUEST</div>
+                        <h4>Free</h4>
+                        <ul>
+                            <li>4 QR code types </li>
+                            <li className="scratch">4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                        </ul>
+
+                        <button className="button-primary button-large">Choose plan</button>
+                    </div>
+
+                    <div className="pricing-item">
+
+                        <div className="pricing-item-heading">GUEST</div>
+                        <h4>Free</h4>
+                        <ul>
+                            <li>4 QR code types </li>
+                            <li className="scratch">4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                        </ul>
+
+                        <button className="button-primary button-large">Choose plan</button>
+                    </div>
+
+                    <div className="pricing-item recommended">
+
+                        <div className="pricing-item-heading">GUEST</div>
+                        <h4>Free</h4>
+                        <ul>
+                            <li>4 QR code types </li>
+                            <li className="scratch">4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                        </ul>
+
+                        <button className="button-primary button-large">Choose plan</button>
+                    </div>
+                    <div className="pricing-item">
+
+                        <div className="pricing-item-heading">GUEST</div>
+                        <h4>Free</h4>
+                        <ul>
+                            <li>4 QR code types </li>
+                            <li className="scratch">4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                            <li>4 QR code types </li>
+                        </ul>
+
+                        <button className="button-primary button-large">Choose plan</button>
+                    </div>
+
+
+
+
+                </div>
+
+
+
+
+
+
+
+            </section>
+
+
+
+
+
+
+
+
+
 
         </div>
 
     )
-
-
 
 
 }
